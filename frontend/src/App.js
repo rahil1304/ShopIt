651d2c10709c1147637219adf08369d6549a86ed
Rahil Sarvaiya <rahil1304@gmail.com>
2020-12-07 22:35:08 +0530

    adding container tags to the main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
     <>
-      <h1>Welcome to ShopIt</h1>
+      <Header />
+      <Container>
+        <h1>Welcome to ShopIt</h1>
+      </Container>
+      <Footer />
     </>
   );
 };
