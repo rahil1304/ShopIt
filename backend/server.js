f083d33aeee81d567348cf80ec7b14523f4458a4
Rahil Sarvaiya <rahil1304@gmail.com>
2020-12-13 15:25:06 +0530

    API to get product by the id

--- backend/server.js
+++ backend/server.js
@@ -11,4 +11,9 @@ app.get("/api/products", (req, res) => {
   res.json(products);
 });
 
+app.get("/api/product/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
 app.listen(5000, console.log("Server Running on port 5000"));
